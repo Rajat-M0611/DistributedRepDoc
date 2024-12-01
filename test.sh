e35054d7cdd239ca9095cf41bbd173bb795556eb
Rajat-M0611 <rajatmallick067@gmail.com>
2024-12-01 21:51:28 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run Python scripts in sequence
-python3 DocClassificationSimCSE_news.py newsgroup20
-python3 DocClassificationSimCSE_rt.py rotten_tomatoes
-python3 DocClassificationSimCSE_db.py dbpedia
